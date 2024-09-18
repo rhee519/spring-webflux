1cccccf22b452f54812487765a826a1c2aba608d
Jiho Rhee <rhee519@gmail.com>
2024-09-18 17:55:46 +0900

    [spring-webflux] Ch7. Cold & Hot Sequence

---
+++ example/src/main/kotlin/com/kakao/example/Example7_2.kt
@@ -0,0 +1,54 @@
+package com.kakao.example
+
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
+import reactor.core.publisher.Flux
+import java.time.Duration
+
+@Slf4j
+class Example7_2 {
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java)
+
+        /**
+         * Hot Sequence(Publisher) Example
+         * - Hot Sequence는 Subscriber가 subscribe할 때마다 새로운 데이터를 생성하지 않고, 이미 생성된 데이터를 공유하는 Publisher
+         * - Hot Sequence는 Subscriber 간에 데이터를 공유함
+         * - delayElements() default scheduler 설정(parallel)에 의해 emit되는 데이터별로 다른 thread에서 실행됨
+         *
+         * output:
+         * 17:42:00.747 [main] INFO com.kakao.example.Example7_2$Companion -- Concert begins!
+         * ----------
+         * 17:42:01.816 [parallel-1] INFO com.kakao.example.Example7_2$Companion -- # Subscriber 1 is watching: Justin Bieber
+         * 17:42:02.819 [parallel-2] INFO com.kakao.example.Example7_2$Companion -- # Subscriber 1 is watching: Ariana Grande
+         * 17:42:03.824 [parallel-3] INFO com.kakao.example.Example7_2$Companion -- # Subscriber 1 is watching: BTS
+         * 17:42:03.825 [parallel-3] INFO com.kakao.example.Example7_2$Companion -- # Subscriber 2 is watching: BTS
+         * 17:42:04.830 [parallel-4] INFO com.kakao.example.Example7_2$Companion -- # Subscriber 1 is watching: Taylor Swift
+         * 17:42:04.830 [parallel-4] INFO com.kakao.example.Example7_2$Companion -- # Subscriber 2 is watching: Taylor Swift
+         * 17:42:05.832 [parallel-5] INFO com.kakao.example.Example7_2$Companion -- # Subscriber 1 is watching: Lady Gaga
+         * 17:42:05.832 [parallel-5] INFO com.kakao.example.Example7_2$Companion -- # Subscriber 2 is watching: Lady Gaga
+         */
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val artists = listOf("Justin Bieber", "Ariana Grande", "BTS", "Taylor Swift", "Lady Gaga")
+            logger.info("Concert begins!")
+
+            val concertFlux = Flux.fromIterable(artists)
+                .delayElements(Duration.ofSeconds(1))
+                .share()
+
+            concertFlux.subscribe {
+                logger.info("# Subscriber 1 is watching: $it")
+            }
+
+            println("----------")
+
+            Thread.sleep(2500)
+            concertFlux.subscribe {
+                logger.info("# Subscriber 2 is watching: $it")
+            }
+
+            Thread.sleep(3000)
+        }
+    }
+}
---
+++ example/src/main/kotlin/com/kakao/example/Example7_3.kt
@@ -0,0 +1,52 @@
+package com.kakao.example
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.core.publisher.Mono
+
+@Slf4j
+class Example7_3 {
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java)
+        private val webClient = WebClient.create()
+        private val objectMapper = ObjectMapper()
+
+        /**
+         * output:
+         * Cold Sequence Example
+         * 17:53:43.409 [reactor-http-nio-2] INFO com.kakao.example.Example7_3$Companion -- # datetime 1: 2024-09-18T17:53:43.273209+09:00
+         * 17:53:45.252 [reactor-http-nio-2] INFO com.kakao.example.Example7_3$Companion -- # datetime 2: 2024-09-18T17:53:45.143743+09:00
+         * ----------
+         * Hot Sequence Example
+         * 17:53:47.267 [reactor-http-nio-2] INFO com.kakao.example.Example7_3$Companion -- # datetime 1: 2024-09-18T17:53:47.157558+09:00
+         * 17:53:49.099 [main] INFO com.kakao.example.Example7_3$Companion -- # datetime 2: 2024-09-18T17:53:47.157558+09:00
+         */
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Cold Sequence Example")
+            val mono = getWorldTime(url = "http://worldtimeapi.org/api/timezone/Asia/Seoul")
+            mono.subscribe { logger.info("# datetime 1: $it") }
+            Thread.sleep(2000)
+            mono.subscribe { logger.info("# datetime 2: $it") }
+            Thread.sleep(2000)
+
+            println("----------")
+            println("Hot Sequence Example")
+            val hotMono = getWorldTime(url = "http://worldtimeapi.org/api/timezone/Asia/Seoul").cache()
+            hotMono.subscribe { logger.info("# datetime 1: $it") }
+            Thread.sleep(2000)
+            hotMono.subscribe { logger.info("# datetime 2: $it") }
+            Thread.sleep(2000)
+        }
+
+        private fun getWorldTime(url: String): Mono<String> {
+            return webClient.get().uri(url).retrieve().bodyToMono(String::class.java).map {
+                val body = objectMapper.readTree(it)
+                val dateTime = body["datetime"].asText()
+                dateTime
+            }
+        }
+    }
+}
